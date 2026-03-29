c9b1a6114af9a469148d139c093f1ec17e54961c
Ítalo Santana <italoeugenio539@gmail.com>
2026-03-29 19:50:10 -0300

    feat: criando utilitario para validar alguns campos do cliente

---
+++ src/main/java/italo/santana/itau_desafio_cartoes/util/ClienteValidator.java
@@ -0,0 +1,35 @@
+package italo.santana.itau_desafio_cartoes.util;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@Component
+public class ClienteValidator {
+
+    public static boolean isValidCpf(String cpf) {
+        return cpf.matches("^[0-9]{11}$");
+    }
+
+    public static boolean isValidUf(String uf){
+        String[] ufBrasil = {
+                "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA",
+                "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN",
+                "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+        };
+
+        for(String ufBrasilItem : ufBrasil){
+            if(uf.toUpperCase().equals(ufBrasilItem)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isIdadeCompativelComDataNascimento(int idadeCliente, LocalDate dataNascimento){
+        int idadeCalculo = Period.between(dataNascimento, LocalDate.now()).getYears();
+        return idadeCliente == idadeCalculo;
+    }
+}
